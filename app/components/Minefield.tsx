2b1f19f5ee3a0a5f63f1449a95deae9afd6e8af2
str4ng3love <normalemailadress@gmail.com>
2023-03-07 13:28:09 +0100

    added random mine placement

--- app/components/Minefield.tsx
+++ app/components/Minefield.tsx
@@ -1,50 +1,68 @@
-'use client'
+"use client";
 
-import { Options } from "./Game"
-import Tile from "./Tile"
-import {useEffect, useState} from 'react'
+import { Options } from "./Game";
+import Tile from "./Tile";
+import { useEffect, useState } from "react";
 
-export default function Minefield(props:{options:Options}){
-  const [field, setField] = useState<{id:number, mine:boolean}[]>()
+export default function Minefield(props: { options: Options }) {
+  const [field, setField] = useState<{ id: number; mine: boolean }[]>();
 
-  const produceField = (x:string, y:string, mines:string) =>{
-    let arr = []
-    let field = parseInt(x)*parseInt(y)
-    for(let i = 0; i < field; i++)
-    {
-      if(parseInt(mines) - i > 0){
-        arr.push({id: i, mine: true})
-      } else {
-        arr.push({id: i, mine: false})
-      }
+  const produceField = (x: string, y: string) => {
+    let arr = [];
+    let field = parseInt(x) * parseInt(y);
+    for (let i = 0; i < field; i++) {
+      arr.push({ id: i, mine: false });
     }
-    for(let i = 0; i < arr.length -1; i--){
-      const j = Math.floor(Math.random() * (i + 1));
-      arr[i], arr[j] = arr[j] , arr[i]
+    return arr;
+  };
+  const addMines = (arr: { id: number; mine: boolean }[], step: number) => {
+    let copy: { id: number; mine: boolean }[] = JSON.parse(JSON.stringify(arr));
+
+    copy.map((el) => (el.mine = false));
+
+    for (let i = step - 1; i > 0; i--) {
+      let index = Math.floor(Math.random() * copy.length);
+      copy[index].mine = true;
     }
-    return arr
-  }
- 
 
+    return copy;
+  };
 
-  useEffect(()=>{
-    
-    setField(produceField(props.options.x, props.options.y, props.options.mines))
-    
-}, [props])
+  useEffect(() => {
+    setField(produceField(props.options.x, props.options.y));
+  }, [props]);
 
-    return(
-      <div className="bg-slate-400 m-1 flex flex-col items-center border-2 border-solid border-b-stone-700 border-r-stone-700  border-l-stone-200  border-t-stone-200">
-        <div className="p-4 flex justify-between">
-            <div className="p-4 mx-1">time?</div>
-            <div className="p-4 mx-1">-reset-</div>
-            <div className="p-4 mx-1">mines left {props.options.mines}</div>
-        </div>
-        <div className={`grid w-fit m-1 grid-cols-[repeat(${props.options.x},16px)] p-1  border-2 border-solid border-b-stone-700 border-r-stone-700  border-l-stone-200  border-t-stone-200`}>
-        {field? field.map(el =>
-          <Tile mine={el.mine} id={el.id} key={el.id}/>
-          ) :<></>}
-        </div>
+  useEffect(() => {
+    console.log(field);
+  }, [field]);
+
+  return (
+    <div className="bg-slate-400 m-1 flex flex-col items-center border-2 border-solid border-b-stone-700 border-r-stone-700  border-l-stone-200  border-t-stone-200">
+      <div className="p-4 flex justify-between">
+        <div className="p-4 mx-1">time?</div>
+        <div className="p-4 mx-1">-reset-</div>
+        <div className="p-4 mx-1">mines left {props.options.mines}</div>
       </div>
-    )
+      <div
+        className={`grid w-fit m-1 grid-cols-[repeat(${props.options.y},16px)] p-1  border-2 border-solid border-b-stone-700 border-r-stone-700  border-l-stone-200  border-t-stone-200`}
+      >
+        {field ? (
+          field.map((el) => <Tile mine={el.mine} id={el.id} key={el.id} />)
+        ) : (
+          <></>
+        )}
+      </div>
+      <button
+        onClick={(e) => {
+          if (field) {
+            console.log("running");
+            setField(addMines(field, parseInt(props.options.mines)));
+          }
+        }}
+        className="p-4 bg-red-400"
+      >
+        test
+      </button>
+    </div>
+  );
 }
--- app/components/Tile.tsx
+++ app/components/Tile.tsx
@@ -1,12 +1,13 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Tile(props:{id:number, mine:boolean}){
     const [ show, setShow] = useState(false)
+
     return(
-        <div onClickCapture={(e)=>setShow(true)} className="text-sm select-none flex justify-center items-center border-solid  bg-slate-400 h-4 w-4 border-2 border-b-stone-700 border-r-stone-700  border-l-stone-200  border-t-stone-200 active:border-b-stone-200 active:border-r-stone-200 active:border-l-stone-700  active:border-t-stone-700 ">
-            {show? <>{props.mine ? "x":<></>}</> :<></>}
+        <div onClickCapture={(e)=>setShow(true)} className="p-1 text-sm select-none flex justify-center items-center border-solid  bg-slate-400 h-4 w-4 border-2 border-b-stone-700 border-r-stone-700  border-l-stone-200  border-t-stone-200 active:border-b-stone-200 active:border-r-stone-200 active:border-l-stone-700  active:border-t-stone-700 ">
+         {props.mine? "x" : ""}
         </div>
     )
 }

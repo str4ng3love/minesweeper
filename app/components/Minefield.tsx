e631a2a32f9d883d4bcffb6d933cbb059305bf2e
str4ng3love <normalemailadress@gmail.com>
2023-03-08 10:15:06 +0100

    added coordinates to field

--- app/components/Minefield.tsx
+++ app/components/Minefield.tsx
@@ -5,36 +5,45 @@ import Tile from "./Tile";
 import { useEffect, useState } from "react";
 
 export default function Minefield(props: { options: Options }) {
-  const [field, setField] = useState<{ id: number; mine: boolean }[]>();
+  const [field, setField] = useState<{ coords: {x:number, y:number}; mine: boolean }[]>();
+  const [gameStart, setGameStart] = useState(false)
 
   const produceField = (x: string, y: string) => {
     let arr = [];
-    let field = parseInt(x) * parseInt(y);
-    for (let i = 0; i < field; i++) {
-      arr.push({ id: i, mine: false });
+    for (let i = 0; i < parseInt(x); i++) {
+      
+      for( let j = 0; j < parseInt(y); j++){
+        arr.push({ coords: {x: i, y: j }, mine: false });
+      }
     }
     return arr;
   };
-  const addMines = (arr: { id: number; mine: boolean }[], step: number) => {
-    let copy: { id: number; mine: boolean }[] = JSON.parse(JSON.stringify(arr));
+  const addMines = (arr: { coords: {x:number, y: number}; mine: boolean }[], step: number) => {
+    let copy: { coords: {x:number, y: number}; mine: boolean }[] = JSON.parse(JSON.stringify(arr));
 
     copy.map((el) => (el.mine = false));
 
     for (let i = step - 1; i > 0; i--) {
       let index = Math.floor(Math.random() * copy.length);
-      copy[index].mine = true;
+      if(copy[index].mine === true){
+        i++
+      } else {
+
+        copy[index].mine = true;
+      }
     }
 
     return copy;
   };
+const checkCoords = (e:React.MouseEvent)=> {
+  console.log(e.currentTarget.children[0].innerHTML)
+  //to do: generate mines on 1st click and make sure that there are no adjacent mines
+}
 
   useEffect(() => {
     setField(produceField(props.options.x, props.options.y));
   }, [props]);
 
-  useEffect(() => {
-    console.log(field);
-  }, [field]);
 
   return (
     <div className="bg-slate-400 m-1 flex flex-col items-center border-2 border-solid border-b-stone-700 border-r-stone-700  border-l-stone-200  border-t-stone-200">
@@ -47,7 +56,7 @@ export default function Minefield(props: { options: Options }) {
         className={`grid w-fit m-1 grid-cols-[repeat(${props.options.y},16px)] p-1  border-2 border-solid border-b-stone-700 border-r-stone-700  border-l-stone-200  border-t-stone-200`}
       >
         {field ? (
-          field.map((el) => <Tile mine={el.mine} id={el.id} key={el.id} />)
+          field.map((el, index) => <Tile mine={el.mine} coords={el.coords} key={index} startFn={checkCoords}/>)
         ) : (
           <></>
         )}
@@ -55,7 +64,6 @@ export default function Minefield(props: { options: Options }) {
       <button
         onClick={(e) => {
           if (field) {
-            console.log("running");
             setField(addMines(field, parseInt(props.options.mines)));
           }
         }}
--- app/components/Tile.tsx
+++ app/components/Tile.tsx
@@ -1,13 +1,26 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 
-export default function Tile(props:{id:number, mine:boolean}){
-    const [ show, setShow] = useState(false)
+export default function Tile(
+  props: { coords: { x: number; y: number }; mine: boolean, startFn: (e: React.MouseEvent) => void;},
+  
+) {
+  const [show, setShow] = useState(false);
+  // useEffect(()=>{
 
-    return(
-        <div onClickCapture={(e)=>setShow(true)} className="p-1 text-sm select-none flex justify-center items-center border-solid  bg-slate-400 h-4 w-4 border-2 border-b-stone-700 border-r-stone-700  border-l-stone-200  border-t-stone-200 active:border-b-stone-200 active:border-r-stone-200 active:border-l-stone-700  active:border-t-stone-700 ">
-         {props.mine? "x" : ""}
-        </div>
-    )
+  //     console.log(props.coords)
+  // }, [props])
+  return (
+    <div
+      onClickCapture={(e) => {
+        props.startFn(e);
+        setShow(true);
+      }}
+      className="p-1 text-sm select-none flex justify-center items-center border-solid  bg-slate-400 h-4 w-4 border-2 border-b-stone-700 border-r-stone-700  border-l-stone-200  border-t-stone-200 active:border-b-stone-200 active:border-r-stone-200 active:border-l-stone-700  active:border-t-stone-700 "
+    >
+      {props.mine && props.coords ? "x" : ``}
+      <span className="absolute invisible">x:{props.coords.x} y:{props.coords.y}</span>
+    </div>
+  );
 }

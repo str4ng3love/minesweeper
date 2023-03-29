06606c18753b2682edca5439ab8d6992e422018c
str4ng3love <normalemailadress@gmail.com>
2023-03-29 10:56:54 +0200

    reveal algorithm implemented

--- app/components/Minefield.tsx
+++ app/components/Minefield.tsx
@@ -17,7 +17,6 @@ export default function Minefield(props: { options: Options }) {
     }[][]
   >([]);
   const [gameStart, setGameStart] = useState(false);
-  
 
   const startGame = (e: React.MouseEvent) => {
     let currentCoords = GetCoords(e);
@@ -27,11 +26,17 @@ export default function Minefield(props: { options: Options }) {
     );
 
     setGameStart(true);
+  };
+
+  const checkTile = (e: React.MouseEvent) => {
+    let currentCoords = GetCoords(e);
+
+    // revealTile(currentCoords);
     RecursiveCheck(field, currentCoords);
   };
 
   // add logic after the game has started here
-  // 
+  //
   const RecursiveCheck = (
     minefield: {
       show: boolean;
@@ -40,38 +45,64 @@ export default function Minefield(props: { options: Options }) {
     }[][],
     coords: { x: number; y: number }
   ) => {
-    revealTile(coords);
+    let newMineField = [...minefield];
+    newMineField = checkArray(newMineField, coords);
+    setField(newMineField);
+  };
 
+  const checkArray = (
+    array: {
+      show: boolean;
+      mine: boolean;
+      adjacentMines: number;
+    }[][],
+    coords: { x: number; y: number }
+  ) => {
+   
+    if (
+      coords.x < 0 ||
+      coords.x > array.length - 1 ||
+      coords.y > array[0].length -1||
+      coords.y < 0
+    ) {
+      return array;
+    }
+    if (array[coords.x][coords.y].mine === true) {
+      revealAll(array);
+      return array;
+    } else if (array[coords.x][coords.y].adjacentMines > 0) {
+      array[coords.x][coords.y].show = true;
+      return array;
+    } else if (array[coords.x][coords.y].show === false) {
+      array[coords.x][coords.y].show = true
+      checkArray(array, { x: coords.x - 1, y: coords.y - 1 });
+      checkArray(array, { x: coords.x - 1, y: coords.y });
+      checkArray(array, { x: coords.x - 1, y: coords.y + 1 });
+      checkArray(array, { x: coords.x, y: coords.y - 1 });
+      checkArray(array, { x: coords.x, y: coords.y + 1 });
+      checkArray(array, { x: coords.x + 1, y: coords.y - 1 });
+      checkArray(array, { x: coords.x + 1, y: coords.y });
+      checkArray(array, { x: coords.x + 1, y: coords.y + 1 });
+      
+    } else {
+      return array;
+    }
+    return array
   };
 
 
-
-  const revealTile = (coords: { x: number; y: number }) => {
-    setField((prev) => {
-      if(prev[coords.x][coords.y].mine === true){
-        prev.map((el)=> el.map(el=> el.show =true))
-      } else {
-        prev[coords.x][coords.y].show = true;
-      }
-      return [...prev];
-    });
+  const revealAll = (
+    array: { show: boolean; mine: boolean; adjacentMines: number }[][]
+  ) => {
+    array.map((x) => x.map((y) => (y.show = true)));
+    return array;
   };
-
-  const checkTile = (e: React.MouseEvent) => {
-    let currentCoords = GetCoords(e);
-  
-    RecursiveCheck(field, currentCoords);
-  };
-
-  //find adjacent tiles and add amount of mines nearby
 
   useEffect(() => {
     setField(GenerateField(props.options.x, props.options.y));
     setGameStart(false);
   }, [props]);
-  useEffect(()=> {
-  
-  }, [field])
+  useEffect(() => {}, [field]);
   return (
     <div className="bg-slate-400 m-1 flex flex-col items-center border-2 border-solid border-b-stone-700 border-r-stone-700  border-l-stone-200  border-t-stone-200">
       <div className="p-4 flex justify-between">
@@ -80,7 +111,7 @@ export default function Minefield(props: { options: Options }) {
         <div className="p-4 mx-1">mines left {props.options.mines}</div>
       </div>
       <div
-        className={`grid w-fit m-1 grid-cols-[repeat(9,16px)] p-1  border-2 border-solid border-b-stone-700 border-r-stone-700  border-l-stone-200  border-t-stone-200`}
+        className={`grid w-fit m-1 grid-cols-[repeat(${props.options.x},16px)] p-1  border-2 border-solid border-b-stone-700 border-r-stone-700  border-l-stone-200  border-t-stone-200`}
       >
         {/* TODO: Add 'show/visible/display' state to 2darray, and pass it as prop to Tile.tsx */}
 
@@ -116,13 +147,19 @@ export default function Minefield(props: { options: Options }) {
           <></>
         )}
       </div>
-      <button 
-      onClick={(e)=>setField(prev => {
-        prev[0][0].show = true
-      
-        return prev
-      })}
-      className="p-4 border-solid border-2 rounded-sm border-white"> click me</button>
+      <button
+        onClick={(e) =>
+          setField((prev) => {
+            prev[0][0].show = true;
+
+            return prev;
+          })
+        }
+        className="p-4 border-solid border-2 rounded-sm border-white"
+      >
+        {" "}
+        click me
+      </button>
     </div>
   );
 }
